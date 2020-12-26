efb941024bb994cfb154d08c2b856d6689d26b7c
manaccac <maximenaccache@gmail.com>
2020-12-26 22:57:15 +0100

    creation de probe 22 par base

--- bot/bot.py
+++ bot/bot.py
@@ -1,26 +1,46 @@
+import sc2
 from sc2 import BotAI, Race
-
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.unit import Unit
+from sc2.units import Units
+from sc2.position import Point2
+from sc2.player import Bot, Computer
 
 class CompetitiveBot(BotAI):
-    NAME: str = "CompetitiveBot"
-    """This bot's name"""
-    RACE: Race = Race.Terran
-    """This bot's Starcraft 2 race.
-    Options are:
-        Race.Terran
-        Race.Zerg
-        Race.Protoss
-        Race.Random
-    """
+	NAME: str = "CompetitiveBot"
+	"""This bot's name"""
+	RACE: Race = Race.Protoss
+	"""This bot's Starcraft 2 race.
+	Options are:
+		Race.Terran
+		Race.Zerg
+		Race.Protoss
+		Race.Random
+	"""
 
-    async def on_start(self):
-        print("Game started")
-        # Do things here before the game starts
+	async def on_start(self):
+		print("Game started")
+		# Do things here before the game starts
 
-    async def on_step(self, iteration):
-        # Populate this function with whatever your bot should do!
-        pass
+	async def on_step(self, iteration):
+		# Populate this function with whatever your bot should do!
+		await self.distribute_workers()
+		await self.build_workers()
 
-    def on_end(self, result):
-        print("Game ended.")
-        # Do things here after the game ends
+		pass
+
+	async def build_workers(self):
+		nexus = self.townhalls.ready.random
+		if (
+			self.can_afford(UnitTypeId.PROBE)
+			and nexus.is_idle
+			and self.workers.amount < self.townhalls.amount * 22
+		):
+			nexus.train(UnitTypeId.PROBE)
+
+
+	def on_end(self, result):
+		print("Game ended.")
+		# Do things here after the game ends
